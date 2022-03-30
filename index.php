b14566851364712ff3da6a48e43fb3c165028c4d
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2022-03-30 20:17:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/MainPageStyle.css">
+    <title>Главная</title>
+</head>
+
+<body>
+<div class="container">
+  <div class="menu">
+      <div class="left"></div>
+      <div class="right"></div>
+  </div>
+  <div class="footer"></div>
+  <div class="mainInfo"></div>
+  <div class="loginContent">
+    <div class="aboutInfo"></div>
+    <div class="regBlock">
+      <div class="welcomeLabel"></div>
+      <div class="welcomeFotter"></div>
+      <div class="regInfoBlock">
+        <div class="logininput"></div>
+      </div>
+    </div>
+    <div class="loginBlock">
+      <div class="loginLabels">
+          
+      <form method="POST">
+            <p><label for="">Введите логин</label></p>
+              <p><input type="text" name="login"></p>
+              <p><label for="">Введите пароль</label></p>
+              <p><input type="text" name="password"></p>
+
+      </div>
+      <div class="loginButton">
+        <div class="loginnerbutton">
+            <input type="submit" value="Войти">
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+
+</html>
---
+++ userpage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/UserPageStyle.css">
+    <title>Страница</title>
+</head>
+<body>
+<div class="container">
+  <div class="menu"></div>
+  <div class="leftMenu"></div>
+  <div class="mainPage">
+    <div class="userAvatar">
+      <div class="avatar">
+        <div class="avatarplace"></div>
+      </div>
+      <div class="actions">
+        <div class="actionButt"></div>
+        <div class="messageButton"></div>
+      </div>
+    </div>
+    <div class="friendList">
+      <div class="FriendLabel"></div>
+      <div class="FriendListOut"></div>
+      <div class="AllFriendsButton"></div>
+    </div>
+    <div class="userList">
+      <div class="userName">
+        <div class="userStatus"></div>
+        <div class="userFullName"></div>
+      </div>
+      <div class="userInfo"></div>
+      <div class="userRedaction">
+        <div class="redButton"></div>
+      </div>
+    </div>
+    <div class="gallery">
+      <div class="PhotoOut"></div>
+      <div class="galleryLabel"></div>
+    </div>
+  </div>
+  <div class="footer"></div>
+</div>
+</body>
+</html>
